d9109237b0dbe444393d1f599a4533ff41395744
AkaneSenri <zhendelan1@gmail.com>
2022-01-03 14:07:36 +0300

    master

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+if ($_SESSION['user']) {
+    header('Location: profile.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+    <!-- Форма авторизации -->
+    <form action="include/signin.php" method="post">
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="Введите свой логин">
+        <label>Пароль</label>
+        <input type="password" name="password" placeholder="Введите пароль">
+        <small id="passHelp" class="form-text text-muted">Пароль должен состоять не менее чем из 6 символов</small>
+        <input type="submit" class="btn btn-dark" value="Войти">
+        <p class="lead">
+            У Вас нет существующего аккаунта? - <a class="text-my-own-color" href="register.php">Зарегистрируйтесь!</a>
+        </p>
+        <?php
+            if ($_SESSION['message']) {
+                echo '
+                <p class="msg"> ' .$_SESSION['message'] . '</p>';
+            }
+            unset($_SESSION['message']);
+        ?>
+    </form>
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+if (!$_SESSION['user']) {
+    header('Location: /');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+    <!-- Форма авторизации -->
+    <form>
+    <img src="<?= $_SESSION['user']['avatar'] ?>" width="200" alt="">
+        <h2 style="margin: 10px 0;"><?= $_SESSION['user']['full_name'] ?></h2>
+        <a href="#"><?= $_SESSION['user']['email'] ?></a>
+        <a href="include/logout.php" class="logout">Выход</a>
+    </form>
+</body>
+</html>
